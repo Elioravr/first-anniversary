93eb11312eaf8d42fdb75476bffc49b278ae8070
Elior Abramoviz Ducker <elior@fb.com>
2020-10-23 02:47:53 +0300

    Initialization of the project

---
+++ data.js
@@ -0,0 +1,46 @@
+const data = {
+    "buttons": {
+        "1": {
+            "background": "images/Dance_0007.jpeg",
+            "buttonText": "מה זה הדבר הזה?",
+            "modalTitle": "מה זה הדבר הזה?",
+            "modalBody": "אז ככה, אנחנו נשואים היום בדיוק שנה, וזאת הייתה שנת קורונה. אין דרך אחרת להגיד את זה - הזמן עצר, ואיתו, גם היכולת שלי להמציא את עצמי מחדש.+אז מה כן אפשר לעשות? אפליקציה! ריכזתי עבורך את כל מה שלמדתי עלייך בשנה הראשונה לנישואין שלנו.+תתכונני לחפירה!+שלך, בעלך. ❤️",
+        },
+        "2": {
+            "background": "images/Dance_0007.jpeg",
+            "buttonText": "מה זה להיות בעלך?",
+            "modalTitle": "זאת אפליקציה לכבודך",
+            "modalBody": "כי אני אוהב אותך מאוד"
+        },
+        "3": {
+            "background": "images/Dance_0007.jpeg",
+            "buttonText": "מה זה?",
+            "modalTitle": "זאת אפליקציה לכבודך",
+            "modalBody": "כי אני אוהב אותך מאוד"
+        },
+        "4": {
+            "background": "images/Dance_0007.jpeg",
+            "buttonText": "מה זה?",
+            "modalTitle": "זאת אפליקציה לכבודך",
+            "modalBody": "כי אני אוהב אותך מאוד"
+        },
+        "5": {
+            "background": "images/Dance_0007.jpeg",
+            "buttonText": "מה זה?",
+            "modalTitle": "זאת אפליקציה לכבודך",
+            "modalBody": "כי אני אוהב אותך מאוד"
+        },
+        "6": {
+            "background": "images/Dance_0007.jpeg",
+            "buttonText": "מה זה?",
+            "modalTitle": "זאת אפליקציה לכבודך",
+            "modalBody": "כי אני אוהב אותך מאוד"
+        },
+        "7": {
+            "background": "images/Dance_0007.jpeg",
+            "buttonText": "מה זה?",
+            "modalTitle": "זאת אפליקציה לכבודך",
+            "modalBody": "כי אני אוהב אותך מאוד"
+        },
+    }
+};
---
+++ index.js
@@ -0,0 +1,80 @@
+class App {
+    constructor() {
+        this.rootElement = document.querySelector('#root');
+        this.buttonsContainer = document.querySelector('#root .buttons-container');
+
+        // Button
+        this.openModalButton = document.querySelector('#root .open-modal-button');
+        this.closeModalButton = document.querySelector('#root .close-modal-button');
+
+        // Modal
+        this.modal = document.querySelector('#root .modal');
+        this.modalTitle = document.querySelector('#root .modal .title');
+        this.modalBody = document.querySelector('#root .modal .body');
+        this.modalImage = document.querySelector('#root .modal .image');
+
+        this.registerEvents();
+        this.initButtons();
+    }
+
+    registerEvents() {
+        // this.openModalButton.addEventListener('click', () => {
+        //     this.openModal();
+        // });
+
+        this.modal.addEventListener('click', () => {
+            console.log('dblclick')
+            this.closeModal();
+        });
+    }
+
+    initButtons() {
+        Object.keys(data.buttons).forEach(key => {
+            const {buttonText, background, modalTitle, modalBody} = data.buttons[key];
+
+            const buttonTitleElement = document.createElement('div');
+            buttonTitleElement.classList.add('button-title');
+            buttonTitleElement.innerHTML = buttonText;
+
+            const buttonElement = document.createElement('div');
+            buttonElement.classList.add('button');
+            buttonElement.style.backgroundImage = `url(${background})`;
+            buttonElement.appendChild(buttonTitleElement);
+
+            const modalBodyComposed = modalBody.split('+').join('</br></br>');
+
+            buttonElement.addEventListener('click', () => {
+                this.setModalContent(modalTitle, modalBodyComposed, background);
+                this.openModal();
+            });
+
+            this.buttonsContainer.appendChild(buttonElement);
+        })
+    }
+
+    preventDefault(e) {
+        e.preventDefault();
+    }
+
+    openModal() {
+        this.modal.classList.add('visible');
+        this.buttonsContainer.classList.remove('visible');
+
+        // window.addEventListener('scroll', (e) => window.scrollTo(0, window.scrollY));
+        // window.addEventListener('touchmove', this.preventDefault, false);
+        // window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    }
+
+    closeModal() {
+        this.modal.classList.remove('visible');
+        this.buttonsContainer.classList.add('visible');
+    }
+
+    setModalContent(title, body, image) {
+        this.modalTitle.innerHTML = title;
+        this.modalBody.innerHTML = body;
+        this.modalImage.style.backgroundImage = `url(${image})`;
+    }
+}
+
+const app = new App();
